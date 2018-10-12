325cc6b30fc45f56a4878e51275de51a9ca6e6b9
Shweta Danej <shweta@prismitworks.com>
2018-10-12 23:12:29 +0530

    updated author archive query

--- classes/class.rtc_main.php
+++ classes/class.rtc_main.php
@@ -10,12 +10,13 @@ if (!class_exists("RTC_Main")) {
             add_action('wp_enqueue_scripts', array($this, 'enqueue_style_and_scripts'));
             add_action('save_post', array($this, 'save_metabox'), 10, 1);
             add_filter('the_content', array($this, 'display_contributors'),99);
+            add_filter( 'posts_where', array($this,'modifiy_author_archive_query' ));
         }
 
         function create_meta_box() {
             $user = wp_get_current_user();
             if (count(array_intersect(array('author', 'administrator', 'editor'), (array) $user->roles)) > 0) {
-                add_meta_box('rtc_metabox', __(RTC_NAME, "rtc"), array($this, "display_meta_box"), array('post','page'), 'side', 'high');
+                add_meta_box('rtc_metabox', __(RTC_NAME, "rtc"), array($this, "display_meta_box"), array('post'), 'side', 'high');
             }
         }
 
@@ -45,6 +46,15 @@ if (!class_exists("RTC_Main")) {
             if (isset($_POST['rt_authors']) && !empty($_POST['rt_authors'])) {
                 update_post_meta($id, 'rt_contributors', $_POST['rt_authors']);
             }
+            
+            foreach ($_POST['rt_authors'] as $key => $value) {
+                $postlist = get_user_meta($value,'rt_contributor_post',true);
+                if(!is_array($postlist)){
+                    $postlist = array();
+                }
+                $postlist[] = $id;
+                update_user_meta($value,'rt_contributor_post' , $postlist);
+            }
         }
 
         function display_contributors($content) {
@@ -63,6 +73,41 @@ if (!class_exists("RTC_Main")) {
                 $content .= sprintf("</ul></div>", __("Contributors", "rtc"));
             }
             return $content;
+        }
+        function modifiy_author_archive_query($where){
+            if(!is_admin() && is_author()){
+//                global $wpdb;
+//                $args = array(
+//                    'post_type' => array('post'),
+//                    'posts_per_page' => -1,
+//                    'fields'          => 'ids',
+//                    'meta_query' => array(
+//                        array(
+//                            'key'     => 'rt_contributors',
+//                            'compare' => 'EXISTS',
+//                        ),
+//                    )
+//                );
+//                $query = get_posts($args);
+//                $postids = array();
+                $author = get_user_by( 'slug', get_query_var( 'author_name' ) );
+                $authorid = $author->ID;
+                $postids = get_user_meta($authorid,'rt_contributor_post',true);
+                
+//                foreach ($query as $key => $value) {
+//                    $meta_values = get_post_meta($value->ID,'rt_contributors',true);
+//                    if(in_array($authodid, $meta_values)){
+//                        $postids[] = $value->ID;
+//                    }
+//                }
+                $postids = implode(",", $postids);
+                $where .= "OR ID IN ($postids)";
+            }
+//            echo "<pre>";
+//                print_r($where);
+//                echo "</pre>";
+//            die;
+            return $where;
         }
 
     }
